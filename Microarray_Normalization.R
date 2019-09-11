8e1c9397094a93c87e807580f8dd669d5c53e2c0
Ruidong Li <rli012@ucr.edu>
2019-09-10 20:58:07 -0700

    Create Microarray_Normalization.R

---
+++ Microarray_Normalization.R
@@ -0,0 +1,20 @@
+
+library(preprocessCore) # quantile normalization
+library(vsn)
+
+eSet <- readRDS(file='data/rData/Sengenics_Immunome_RawMean_eSet.rds')
+phenoData <- pData(eSet)
+exprData <- exprs(eSet)
+
+### Quantile Normalization
+exprDataQuantileNorm <- normalize.quantiles(exprData)
+colnames(exprDataQuantileNorm) <- colnames(exprData)
+rownames(exprDataQuantileNorm) <- rownames(exprData)
+
+### Vsn Normalization
+exprDataVsnNorm <- justvsn(exprData)
+meanSdPlot(exprDataVsnNorm)
+
+### Lowess/Loess
+
+### Median Normalization
