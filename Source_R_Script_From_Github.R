a227b08d293e0712f50a3fbdcbe4219057a2213b
Ruidong Li <rli012@ucr.edu>
2019-09-12 22:24:42 -0700

    Create Source_R_Script_From_Github.R

---
+++ Source_R_Script_From_Github.R
@@ -0,0 +1,4 @@
+ibrary(devtools)
+source_url('https://raw.github.com/rli012/GDCRNATools/master/R/gdcParseMetadata.R')
+
+# NOT https://github.com/Jialab-UCR/GDCRNATools/blob/master/R/gdcParseMetadata.R
