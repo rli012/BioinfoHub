d3b9722e660341c975d5711f0684c65ecf9089b3
Ruidong Li <rli012@ucr.edu>
2019-10-19 15:05:29 -0700

    Add files via upload

---
+++ Slurm_MultiQC.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --mem=16G
+#SBATCH --time=10:00:00
+#SBATCH --output=MultiQC.stdout
+#SBATCH -p batch
+
+export PYTHONPATH=/rhome/rli012/bigdata/G/env/lib/python2.7/site-packages
+
+multiqc fastpQC/
+
+echo "done"
+
+
+### Installation
+
+#virtualenv env
+#source env/bin/activate
+#export PYTHONPATH=`pwd`/env/lib/python2.7/site-packages
+# #pip install --upgrade pip
+#pip install multiqc
+
+#scp -r FastQC/ rli012@cluster.hpcc.ucr.edu:~/bigdata/G/
+
