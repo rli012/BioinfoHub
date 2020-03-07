f5cbb5d9a814bf410d7530acbf7cba04722058b4
Ruidong Li <rli012@ucr.edu>
2020-03-06 20:38:30 -0800

    Create Install_R_package_on_Server.R

---
+++ Install_R_package_on_Server.R
@@ -0,0 +1,5 @@
+.libPaths('~/bigdata/software/Rlibrary')
+
+install.packages('Matrix', lib='~/bigdata/software/Rlibrary', repos='http://cran.stat.ucla.edu')
+
+library('Matrix', lib.loc='~/bigdata/software/Rlibrary')
